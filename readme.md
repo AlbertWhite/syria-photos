0aa42f414968034be898ca387cdc489a647857c5
Yuebai XU <y.xu@gop.link>
2017-12-07 18:20:46 +0100

    update readme

--- readme.md
+++ readme.md
@@ -8,3 +8,6 @@ It is a bit sad to see how the cultural relics and people life are ruined. Hope 
 ##### Image source:
 1. http://www.bbc.com/news/world-middle-east-38396747
 2. https://www.theatlantic.com/photo/2016/12/aleppo-before-the-war/511424/
+
+##### Stack
+Gulp (minify, parse scss, build production), Jquery(old but still handy)
--- src/main.js
+++ src/main.js
@@ -36,8 +36,6 @@ $(document).ready(() => {
 	//set one carousel width
 	var window_width = setWindowWidth();
 	var currentCarousel = 1;
-	var bgColor = 'rgb(0,0,0)';
-	$('.body').css('background-color', bgColor);
 
 	$('.title-container .description').text(PHOTOS[currentCarousel - 1].name);
 
@@ -45,10 +43,6 @@ $(document).ready(() => {
   $(".img-div").mousemove((e) => {
     var ratio = e.pageX - parseInt($(".img-container").css("margin-left"));
     
-    bgColor = `rgb(${parseInt((976 - ratio)/10)},0,0)`
-		$('body').css('background-color', bgColor);
-
-
     $('.img-1').css('width',`${ratio}px`);
     $('.img-2').css('width',`${976 - ratio}px`);
   	$('.bar').css('left', `${ratio}px`);
@@ -100,9 +94,6 @@ $(document).ready(() => {
   	$('.img-1').css('width','50%');
   	$('.img-2').css('width','50%');
   	$('.bar').css('left', '50%');
-
-  	var bgColor = 'rgb(0,0,0)';
-		$('body').css('background-color', bgColor);
 
 		$('.title-container .description').text(PHOTOS[currentCarousel - 1].name);
 
